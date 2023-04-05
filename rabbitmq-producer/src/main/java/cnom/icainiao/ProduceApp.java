39cf70c20579a704ea9e3617e31e58423b4c2182
IcainiaoCT <10686679+icainiaoct@user.noreply.gitee.com>
2023-04-05 10:14:42 +0800

    Initial commit

---
+++ rabbitmq-producer/src/main/java/cnom/icainiao/ProduceApp.java
@@ -0,0 +1,64 @@
+package cnom.icainiao;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class ProduceApp {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory=new ConnectionFactory();
+        factory.setHost("192.168.116.147");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+
+        //3. 创建连接 Connection
+        Connection connection = factory.newConnection();
+        //4. 创建Channel
+        Channel channel = connection.createChannel();
+        //5. 创建队列Queue
+        /*
+        queueDeclare(String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments)
+        参数：
+            1. queue：队列名称
+            2. durable:是否持久化，当mq重启之后，还在
+            3. exclusive：
+                * 是否独占。只能有一个消费者监听这队列
+                * 当Connection关闭时，是否删除队列
+                *
+            4. autoDelete:是否自动删除。当没有Consumer时，自动删除掉
+            5. arguments：参数。
+
+         */
+        //如果没有一个名字叫hello_world的队列，则会创建该队列，如果有则不会创建
+        channel.queueDeclare("hello_world",true,false,false,null);
+        /*
+        basicPublish(String exchange, String routingKey, BasicProperties props, byte[] body)
+        参数：
+            1. exchange：交换机名称。简单模式下交换机会使用默认的 ""
+            2. routingKey：路由名称
+            3. props：配置信息
+            4. body：发送消息数据
+
+         */
+
+        String body = "hello rabbitmq~~~";
+
+        //6. 发送消息
+        channel.basicPublish("","hello_world",null,body.getBytes());
+
+
+        //7.释放资源
+        channel.close();
+        connection.close();
+
+    }
+
+
+
+}
